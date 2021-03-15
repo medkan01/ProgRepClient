1b9e821029e0c598bea7e2b20c44f4587aba5058
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2021-03-15 21:09:15 +0100

    add the second game

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,40 +1,27 @@
 package client;
 
 import java.net.URL;
-import java.rmi.Naming;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import modele.interfaceRMI.InterfacePendu;
 
 public class Client extends Application{
 
     public void start(Stage primaryStage) {
         try {
-            URL fxmlURL=getClass().getResource("../vue/PenduVue.fxml");
+            URL fxmlURL=getClass().getResource("../vue/MenuPrincipalVue.fxml");
             FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
             Node root = fxmlLoader.load();
             Scene scene = new Scene((AnchorPane) root, 720, 480);
             primaryStage.setScene(scene);
-            primaryStage.setTitle("Pendu");
+            primaryStage.setTitle("Accueil");
             primaryStage.show();
             primaryStage.setResizable(false);
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-    public static void main(String[] args) {
-        try {
-            
-            String hote = args[0];
-            int port = Integer.parseInt(args[1]);
-            InterfacePendu pendu = (InterfacePendu) Naming.lookup("rmi://" + hote + ":" + port + "/Pendu");
-            launch(args);
-        } catch (Exception e) {
-            System.out.println("Une erreur est survenue:\n" + e);
         }
     }
 }
